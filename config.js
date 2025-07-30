54c14dedf9fe955935840320205378046a7e5602
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-30 11:40:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'İnönü Gezisi/ Taksim Gezi Park',
+    title: 'Kentsel Boşluğun Üretimi:İnönü Gezisi (Taksim Gezi Park) / Production of Urban Voids:İnönü Gezisi (Taksim Gezi Park)',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
